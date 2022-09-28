fc06696247431bfe390a2d9e37798b7a6bea25dd
amendelson <mendelson.inbox@gmail.com>
2022-09-28 15:56:03 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ms-fsa',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
