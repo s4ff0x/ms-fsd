fb19dde449a2432f03a7c6c6994ab39dfb1954a7
amendelson <mendelson.inbox@gmail.com>
2022-10-07 10:30:39 +0300

    refactoring

---
+++ src/shared/ui/button/index.ts
@@ -0,0 +1,34 @@
+import { IonButton } from "@ionic/react";
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+export type ButtonProps = {
+  onClick?: (data: any) => void;
+  children?: ReactNode;
+  outline?: boolean;
+  type?: any;
+  disabled?: boolean;
+};
+
+export const ButtonStyled = styled(IonButton)<ButtonProps>`
+  --padding-start: 2rem;
+  --padding-end: 2rem;
+  --color: var(--dark);
+  --background: var(--buttonGradient);
+  --background-activated-opacity: 0;
+  --border-radius: var(--borderRadiusButton);
+  flex-shrink: 0;
+
+  height: 3.3rem;
+  font-size: 1rem;
+  display: inline-flex;
+  font-weight: 600;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: normal;
+  text-transform: none;
+
+  &:active {
+    opacity: 0.7;
+  }
+`;
